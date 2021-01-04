7153a4323d2891e8ec29033714787ac5da0ffad2
rongbang <rongbang.ye@gmail.com>
2021-01-03 23:50:28 -0800

    first commit

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,40 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("bookCount")
+          .populate("savedBooks");
+      }
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      //
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError(
+          "Incorrect credentials: Email not found "
+        );
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError(
+          "Incorrect credentials: Incorrect password"
+        );
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
